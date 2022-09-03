4ed1d5eb5036d92968813b616e20d62efc3019d4
lanquanxin <1109691453@qq.com>
2022-09-03 23:59:49 +0800

    实现禁用EditText上下文菜单（copy share paste）

--- app/src/main/java/com/example/uiwidget/MainActivity.kt
+++ app/src/main/java/com/example/uiwidget/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.example.uiwidget
 
 import android.os.Bundle
-import android.view.View
+import android.view.*
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import com.example.uiwidget.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -17,13 +19,20 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        disableEditTextContextMenu(binding.editText)        //禁用EditText上下文菜单（copy share paste）
+
+
+
 
         binding.buttonButton1.setOnClickListener(this)
 
     }
 
+
+    /* 重写onClick，实现监测按键按下状态，实现对应响应 */
     override fun onClick(p0: View?) {
         when (p0?.id) {
             R.id.button_Button1 -> {
@@ -31,7 +40,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             }
         }
     }
-
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
@@ -55,5 +63,25 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 //                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
 //        }
     }
+
+    /* 禁用EditText上下文菜单（copy share paste）
+    *  https://www.geeksforgeeks.org/how-to-disable-copy-functionality-in-edittext-in-android/ */
+    fun disableEditTextContextMenu(EditText: EditText) {
+        // EditText禁用复制功能
+        EditText.customSelectionActionModeCallback = object : ActionMode.Callback {
+            override fun onCreateActionMode(p0: ActionMode?, p1: Menu?): Boolean {
+                return false
+            }
+            override fun onPrepareActionMode(p0: ActionMode?, p1: Menu?): Boolean {
+                return false
+            }
+            override fun onActionItemClicked(p0: ActionMode?, p1: MenuItem?): Boolean {
+                return false
+            }
+            override fun onDestroyActionMode(p0: ActionMode?) {
+            }
+        }
+    }
+
 
 }
